25d62028e716298486a0cd9114dbbbe2241da778
Benjay20 <139428789+Benjay20@users.noreply.github.com>
2024-10-31 14:33:00 +0800

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,5 @@
+// Control background music volume
+document.addEventListener('DOMContentLoaded', function() {
+    const bgMusic = document.getElementById('bgMusic');
+    bgMusic.volume = 0.4;  // Set volume to 40%
+  });
